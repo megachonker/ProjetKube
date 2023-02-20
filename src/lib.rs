c24d70a9758027c05c5193c4c7056a02833f8e84
L <>
2023-02-20 19:52:25 +0100

    2 library

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,2 @@
-mod viewport;
-pub use crate::viewport::run;
 
-mod primitive;
-pub use crate::primitive::build;
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use kube::run;
+use viewport::run;
+use primitive::add;
 
 fn main() {
+    add(2, 4);
     pollster::block_on(run());
 }
