4b69e5c2f833bc1100ba3d1c4b3e9199121079a3
ereeee <charles.gilbert@hotmail.fr>
2023-02-21 19:50:46 +0100

    cube réalisé pré print

--- src/primitive.rs
+++ src/primitive.rs
@@ -7,28 +7,23 @@ fn create_point( x: i32, y: i32,z: i32) -> Point {
 }
 
 
-fn create_maille(point0: Point, point1: Point, point2: Point, point3: Point) -> Maille {
-    Maille { point0, point1, point2, point3,point4, point5, point6, point7 }
+fn create_maille(point0: Point,point1: Point,point2: Point,point3: Point,point4: Point,point5: Point,point6: Point,point7: Point) -> Maille {
+     Maille { point0, point1, point2, point3,point4, point5, point6, point7 }
 }
 
-fn pmaille(maille : Maille){
-    ppoint(maille.point0);
-    ppoint(maille.point1);
-    ppoint(maille.point2);
-    ppoint(maille.point3);
 
 
 fn main() {
-    let point0 = create_point(-1, -1,  0);
-    let point1 = create_point(-1, 1,  0);
-    let point2 = create_point(1, -1,  1);
-    let point3 = create_point(1, 1,  0);
-    let point4 = create_point(-1, -1,  1);
-    let point5 = create_point(-1, 1,  1);
-    let point6 = create_point(1, -1,  1);
-    let point7 = create_point(1, 1,  0);
+    let point0:Point = create_point(-1, -1,  0);
+    let point1:Point = create_point(-1, 1,  0);
+    let point2:Point = create_point(1, -1,  1);
+    let point3:Point = create_point(1, 1,  0);
+    let point4:Point = create_point(-1, -1,  1);
+    let point5:Point = create_point(-1, 1,  1);
+    let point6:Point = create_point(1, -1,  1);
+    let point7:Point = create_point(1, 1,  0);
  
-    let maille:Maille = (point0,point1,point2,point3,point4,point5,point6,point7);
+    let maille:Maille=create_maille(point0 ,point1 ,point2 ,point3 ,point4 ,point5 ,point6 ,point7 );
     
 
  }
