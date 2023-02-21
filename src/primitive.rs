772bc77db4f22cda34e3793980b95226dee4abd1
megachonker <>
2023-02-21 19:49:00 +0100

    feat(cube) create cub and print

--- src/primitive.rs
+++ src/primitive.rs
@@ -7,8 +7,12 @@ fn create_point( x: i32, y: i32,z: i32) -> Point {
 }
 
 
-fn create_maille(point0: Point, point1: Point, point2: Point, point3: Point) -> Maille {
+fn create_maille(point0: Point, point1: Point, point2: Point, point3: Point, point4: Point, point5: Point, point6: Point, point7: Point) -> Maille {
     Maille { point0, point1, point2, point3,point4, point5, point6, point7 }
+}
+
+fn ppoint(point: Point){
+    println!("X:{}\tY:{}\tZ:{}",point.x,point.y,point.z);
 }
 
 fn pmaille(maille : Maille){
@@ -16,9 +20,9 @@ fn pmaille(maille : Maille){
     ppoint(maille.point1);
     ppoint(maille.point2);
     ppoint(maille.point3);
+}
 
-
-fn main() {
+fn cube() -> Maille{
     let point0 = create_point(-1, -1,  0);
     let point1 = create_point(-1, 1,  0);
     let point2 = create_point(1, -1,  1);
@@ -27,8 +31,12 @@ fn main() {
     let point5 = create_point(-1, 1,  1);
     let point6 = create_point(1, -1,  1);
     let point7 = create_point(1, 1,  0);
- 
-    let maille:Maille = (point0,point1,point2,point3,point4,point5,point6,point7);
-    
+    create_maille(point0,point1,point2,point3,point4,point5,point6,point7)
+}
 
+pub fn test() {
+
+    let cubee = cube();
+    pmaille(cubee);    
+    
  }
