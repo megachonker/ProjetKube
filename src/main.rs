dfeb75c61cdae172af6935c4d51f65b83f19e381
megachonker <>
2023-02-22 01:13:50 +0100

    opt(primitive) connected brain

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ mod primitive;
 pub use crate::viewport::run;
 pub use crate::primitive::test;
 
+
 fn main() {
+
+
+    // print the mesh
     test();
     // pollster::block_on(run());
 }
--- src/primitive.rs
+++ src/primitive.rs
@@ -1,44 +1,30 @@
-struct Point { x: i32,y: i32,z: i32,}
-struct Maille {point0: Point,point1: Point,point2: Point,point3: Point,point4: Point,point5: Point,point6: Point,point7: Point,}
-
-
-fn create_point( x: i32, y: i32,z: i32) -> Point {
-    Point {x, y,z }
+struct Vertex<'b> { x: i32,y: i32,z: i32,
+                    face_list:&'b [Face<'b>]
 }
 
-
-fn create_maille(point0: Point,point1: Point,point2: Point,point3: Point,point4: Point,point5: Point,point6: Point,point7: Point) -> Maille {
-     Maille { point0, point1, point2, point3,point4, point5, point6, point7 }
+impl Vertex<'_> {
+    fn print(&self){
+        print!("x:{} y:{} z:{}",self.x,self.y,self.z);
+    }
 }
+struct Face<'a> {vertex:[Vertex<'a>;3]}
 
-fn ppoint(point: Point){
-    println!("X:{}\tY:{}\tZ:{}",point.x,point.y,point.z);
-}
-
-fn pmaille(maille : Maille){
-    ppoint(maille.point0);
-    ppoint(maille.point1);
-    ppoint(maille.point2);
-    ppoint(maille.point3);
-}
-
-fn cube() -> Maille{
-    let point0 = create_point(-1, -1,  0);
-    let point1 = create_point(-1, 1 ,  0);
-    let point2 = create_point(1 , -1,  1);
-    let point3 = create_point(1 , 1 ,  0);
-    let point4 = create_point(-1, -1,  1);
-    let point5 = create_point(-1, 1 ,  1);
-    let point6 = create_point(1 , -1,  1);
-    let point7 = create_point(1 , 1 ,  0);
-    create_maille(point0,point1,point2,point3,point4,point5,point6,point7)
+impl Face<'_> {
+    fn print(&self){
+        for vertex in &self.vertex {
+            vertex.print(); println!();
+        }
+    }
 }
 
 pub fn test() {
+    let a = Vertex {x: 1,y: 2, z:3, face_list: &[]};
+    let b = Vertex {x: 11,y: 11, z:11, face_list: &[]};
+    let c = Vertex {x: 22,y: 22, z:22, face_list: &[]};
 
-    let cubee = cube();
-    pmaille(cubee);    
-    
- }
+    let face = Face {vertex: [a,b,c]};
+
+    face.print();
+}
 
  //cube => 4 face => 1 face ces N point 
