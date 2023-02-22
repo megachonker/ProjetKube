2d904e025ec66225a5721250cb5c14ed525c5d08
megachonker <>
2023-02-22 03:37:38 +0100

    feat(add_vertex) ajout vertex
    buguer ne choisie pas encore le plus proche

--- src/primitive.rs
+++ src/primitive.rs
@@ -1,15 +1,53 @@
-struct Vertex<'b> { x: i32,y: i32,z: i32,
-                    face_list:&'b [Face<'b>]
+use std::{vec};
+use nalgebra::{Point3};
+
+struct Vertex<'a> { coord: Point3<f64>,
+                    face_list:Vec<&'a Face<'a>>
 }
 
 impl Vertex<'_> {
+    //calculate distance
+    fn dist(&self,vertex: &Vertex) -> f64{
+        let distance = (self.coord - vertex.coord).norm();
+        print!("Distance {}",&distance);
+        distance
+    }
+
     fn print(&self){
-        print!("x:{} y:{} z:{}",self.x,self.y,self.z);
+        print!("x:{} y:{} z:{}",self.coord.x,self.coord.y,self.coord.z);
     }
 }
-struct Face<'a> {vertex:[Vertex<'a>;3]}
+struct Face<'a> {vertex:[&'a Vertex<'a>;3]}
 
 impl Face<'_> {
+    /// ajoute une face a la distance la plus éloigner
+    /// 
+    /// A
+    /// | \
+    /// |   C
+    /// | /
+    /// B     D(new vertex)
+    /// 
+    /// A
+    /// | \
+    /// |   C
+    /// | /  \
+    /// B  -- D(new face C-B-D)
+    /// 
+    /// calcule distance 
+    fn add_vertex<'a>(&'a self, azer: &'a Vertex) -> Face {
+        let mut ret;
+
+        let A = azer.dist(self.vertex[0]);
+        let B = azer.dist(self.vertex[1]);
+        let C = azer.dist(self.vertex[2]);
+        if A > B && B > C{ ret = Face {vertex: [self.vertex[0],self.vertex[1],&azer]}; }
+        else if B > A && A > C{ ret = Face {vertex: [self.vertex[1],self.vertex[2],&azer]}; }
+        else if C > A && A > B{ ret = Face {vertex: [self.vertex[2],self.vertex[1],&azer]}; }
+        else { ret = Face {vertex: [&azer,&azer,&azer]};}
+        ret
+    }
+
     fn print(&self){
         for vertex in &self.vertex {
             vertex.print(); println!();
@@ -17,14 +55,33 @@ impl Face<'_> {
     }
 }
 
+struct Mesh<'a>{
+    vertices: Vec<Vertex<'a>>,
+    faces: Vec<Face<'a>>
+}
+
+// impl Mesh<'_> {
+//     fn add_face(&mut self, face: Face) {
+//         self.faces.push(face);
+//     }
+// }
+
 pub fn test() {
-    let a = Vertex {x: 1,y: 2, z:3, face_list: &[]};
-    let b = Vertex {x: 11,y: 11, z:11, face_list: &[]};
-    let c = Vertex {x: 22,y: 22, z:22, face_list: &[]};
+    
+    let a = Vertex {coord: Point3::new(1.0,2.0,3.0), face_list: vec![]};
+    let b = Vertex {coord: Point3::new(11.0,22.0,33.0), face_list: vec![]};
+    let c = Vertex {coord: Point3::new(11.0,21.0,31.0), face_list: vec![]};
 
-    let face = Face {vertex: [a,b,c]};
+    let face = Face {vertex: [&a,&b,&c]};
+    let d = Vertex {coord: Point3::new(1.1,2.2,3.3), face_list: vec![]};
 
+    let face2 = face.add_vertex(&d);
+    //  Face {vertex: [&b,&c,&d]};
+
+    println!("face1");
     face.print();
+    println!("face2");
+    face2.print();
 }
 
  //cube => 4 face => 1 face ces N point 
